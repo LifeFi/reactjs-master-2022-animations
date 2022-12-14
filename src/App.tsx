3fcc1208ef234508256696025a0e812c3c69fcf0
topcircle <topcircle@gmail.com>
2022-12-14 11:35:50 +0900

    8.16 수정. 안쓰는 circle 제거

--- src/App.tsx
+++ src/App.tsx
@@ -45,13 +45,6 @@ const overlay = {
   exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
 };
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 function App() {
   const [id, setId] = useState<null | string>(null);
 
